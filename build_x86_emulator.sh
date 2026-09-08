e8783751b1b467c5778f38c8ea31c19f07ace393
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2026-09-09 04:03:10 +0530

    Update repo init command with additional options

--- build_x86_emulator.sh
+++ build_x86_emulator.sh
@@ -2,7 +2,7 @@
 set -e
 
 # 1. Initialize XephiraOS repository manifest
-repo init -u https://github.com/XephiraOS/android.git -b lineage-23.2 --git-lfs; \
+repo init -u https://github.com/XephiraOS/android.git -b lineage-23.2 --git-lfs --no-clone-bundle --depth=1; \
 
 # 2. Sync repositories
 /opt/crave/resync.sh; \
